f9d68ac705d47c703f7b027577378a76b1201ef7
shearstress021 <shearstress021@gmail.com>
2025-11-03 09:57:21 -0800

    added motion and footer

--- src/main/java/com/example/goalie/HomeContoller.java
+++ src/main/java/com/example/goalie/HomeContoller.java
@@ -1,11 +1,14 @@
 package com.example.goalie;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@AllArgsConstructor
 public class HomeContoller {
-    @RequestMapping("/")
+    @GetMapping("/")
     public String Home(){
-        return "Main";
+        return "home";
     }
 }
---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,22 @@
+AOS.init({
+    duration: 800,
+    easing: 'ease-in-out',
+    once: true
+});
+
+feather.replace();
+
+// Initialize Vanta.js globe effect
+VANTA.GLOBE({
+    el: "#vanta-globe",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0x3a82ff,
+    backgroundColor: 0x1665d8,
+    size: 0.8
+});
