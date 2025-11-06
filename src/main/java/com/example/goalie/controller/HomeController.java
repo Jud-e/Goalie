fab1cdc52f62eb6bda60a2e08c3f21e2cb68e96b
Jude Enuanwa <lenuanwa@gmail.com>
2025-11-06 12:16:24 -0800

    model and repositories

--- src/main/java/com/example/goalie/controller/HomeController.java
+++ src/main/java/com/example/goalie/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.example.goalie;
+package com.example.goalie.controller;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
---
+++ src/main/java/com/example/goalie/model/Team.java
@@ -0,0 +1,23 @@
+package com.example.goalie.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+public class Team {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<PlayerTeam> playerTeams;
+
+    @OneToMany(mappedBy = "match")
+    private List<Match> matches;
+}
