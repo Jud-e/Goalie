6cf9aa8a02b4df6c0083a5c075c7402728fefa5f
Jude Enuanwa <lenuanwa@gmail.com>
2025-11-07 23:11:53 -0800

    HomeController updates and Appservice creation

---
+++ src/main/java/com/example/goalie/config/AppService.java
@@ -0,0 +1,39 @@
+package com.example.goalie.config;
+
+import com.example.goalie.model.User;
+import com.example.goalie.repository.*;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AppService {
+    private final UserRepository userRepository;
+    private final TournamentRepository tournamentRepository;
+    private final TeamRepository teamRepository;
+    private final PlayerTeamRepository playerTeamRepository;
+    private final NotificationRepository notificationRepository;
+    private final MessagingRepository messagingRepository;
+    private final MatchRepository matchRepository;
+
+    public AppService(UserRepository userRepository,
+                      TournamentRepository tournamentRepository,TeamRepository teamRepository,
+                      PlayerTeamRepository playerTeamRepository,NotificationRepository notificationRepository,
+                      MessagingRepository messagingRepository,
+                      MatchRepository matchRepository) {
+        this.userRepository = userRepository;
+        this.tournamentRepository = tournamentRepository;
+        this.teamRepository = teamRepository;
+        this.playerTeamRepository = playerTeamRepository;
+        this.notificationRepository = notificationRepository;
+        this.messagingRepository = messagingRepository;
+        this.matchRepository = matchRepository;
+    }
+
+    public User createUser(User user){
+        user.setSubscription(false);
+        return userRepository.save(user);
+    }
+
+    public User getUserByEmail(String email){
+        return userRepository.findByEmail(email).orElse(null);
+    }
+}
--- src/main/java/com/example/goalie/controller/HomeController.java
+++ src/main/java/com/example/goalie/controller/HomeController.java
@@ -1,10 +1,15 @@
 package com.example.goalie.controller;
+import com.example.goalie.config.AppService;
 import com.example.goalie.model.User;
+import jakarta.servlet.http.HttpSession;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,25 +17,58 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping
 @RequiredArgsConstructor
 public class HomeController {
+    private final AppService service;
+
     @GetMapping("/")
     public String Home(){
-        return "home";
+        return "redirect:/signup";
     }
-
+//signup functions
     @GetMapping("/signup")
-    public String Signup(Model model){
+    public String showSignup(Model model){
         model.addAttribute("user", new User());
         return "signup";
     }
-
+    @PostMapping("/signup")
+    public String signupSubmit(@ModelAttribute("user")  User user, BindingResult br){
+        if (br.hasErrors()){
+            return "signup";
+        }
+        user.setSubscription(false);
+        user.setTournament(null);
+//        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        service.createUser(user);
+        return "redirect:/login";
+    }
+//login functions
     @GetMapping("/login")
-    public String Login(){
+    public String showLogin(Model model){
+        model.addAttribute("user", new User());
         return "login";
     }
+
     @PostMapping("/login")
-    public String login(){
-        return "redirect:/";
+    public String loginSubmit(@ModelAttribute("user")  User user, Model model, HttpSession session){
+        User userExisting = service.getUserByEmail(user.getEmail());
+        if (userExisting != null && userExisting.getPassword().equals(user.getPassword())){
+            session.setAttribute("loggedInUser",userExisting);
+            return "redirect:/home";
+        }
+        else {
+            model.addAttribute("error", "Invalid username or password");
+            return "login";
+        }
     }
+//homepage
+    @GetMapping("/home")
+    public String home(HttpSession session, Model model) {
+        User user = (User) session.getAttribute("loggedInUser");
+        if(user == null) {
+            return "redirect:/login";
+        }
+    model.addAttribute("user", user);
+    return "home";
+}
 
     @GetMapping("/about")
     public String About(){
